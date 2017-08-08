e7d6c3d7f4f4a99d861cd04f3931c161751405f8
Sono Shah <sonoshah@gmail.com>
2017-08-08 08:54:00 -0700

    Starter file

---
+++ posc149-lab1-starterfile.R
@@ -0,0 +1,41 @@
+# POSC 149 Lab 1
+# ENTER YOUR NAME HERE
+# ENTER THE DATE HERE
+
+# Go to the online tutorial to follow along 
+
+# Installing Packages 
+install.packages("tidyverse",repos = "http://cran.us.r-project.org")
+library(tidyverse)
+
+# Load in Election Data
+electiondta<-read_csv('https://raw.githubusercontent.com/sonoshah/POSC149/master/county-level-returns-withstate.csv')
+
+
+# As you work through the lab you will write your answers and code here then turn it in on iLearn
+# Make sure anything that is not your code as a # at the start of the line so R knows not to run it.
+# Don't forget to save!!!
+# Write your answers using the format below
+
+
+# Part I: Working with the Data
+# Q1 Code:
+
+
+#Q1 Answer
+# Type in your answer here
+
+
+# Q2A Code:
+
+
+
+#Q2A Answer
+# Type in your answer here
+
+# Q2B Code:
+
+
+#Q2B Answer
+# Type in your answer here
+
